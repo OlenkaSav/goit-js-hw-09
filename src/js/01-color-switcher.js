86f2c324d3c89063346a5a55ee91e4b3e23b3646
OlenkaSav <94956727+OlenkaSav@users.noreply.github.com>
2022-05-25 17:14:05 +0300

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,48 @@
+refs ={
+    buttonStart: document.querySelector('button[data-start]'),
+    buttonStop: document.querySelector('button[data-stop]'),
+    bodyElement: document.querySelector('body'),
+    buttons:  document.querySelectorAll('button'),
+};
 
+refs.buttonStart.addEventListener('click', onButtonStart);
+refs.buttonStop.addEventListener('click', onButtonStop);
+
+function onButtonStart(){
+    console.log('hi');
+  refs.buttonStart.disabled=true;
+    colorChange=setInterval(()=>{
+refs.bodyElement.style.backgroundColor = getRandomHexColor()},1000);
+}
+
+function onButtonStop(){
+    clearInterval(colorChange);
+    refs.buttonStart.disabled=false;
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+//   refs.bodyElement.style.cssText =`display: flex;
+//   align-items: center;
+//   justify-content: space-around;
+
+//   ` 
+//   refs.bodyElement.style.margin='auto'; 
+
+//   refs.buttons.style.cssText = `width: 140px;
+//   height: 60px;
+//   font-size: 30px;
+//   display: flex;
+//   align-Items: center;
+//   justify-content: center;
+//   `;
+refs.buttons.forEach(element => {element.style.cssText = `width: 140px;
+ height: 60px;
+font-size: 30px;
+margin-left: 60px;
+`
+    
+});
+console.log(refs.buttons);
