f1477949ae406c2ebb00ab6f90d98f02805da888
OlenkaSav <94956727+OlenkaSav@users.noreply.github.com>
2022-05-30 18:13:34 +0300

    task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,58 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const refs ={
+  form: document.querySelector('.form'),
+  inputDelay: document.querySelector('input[name="delay"]'),
+  inputStap: document.querySelector('input[name="step"]'),
+  inputAmount: document.querySelector('input[name="amount"]'),
+  btnStart: document.querySelector('button')
+}
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(evt){
+    evt.preventDefault();
+    refs.btnStart.disabled=true;
+    let positionNumber=1;
+  const delayValue = Number(refs.inputDelay.value)*1000;
+  const positionAmount =Number(refs.inputAmount.value);
+  const stapValue = Number(refs.inputStap.value)*1000;
+  let intervalToShow=delayValue;
+ 
+  const intervalId=setInterval(()=>{
+    if (positionNumber>positionAmount){
+     clearInterval(intervalId);
+     return;
+    } else{
+     createPromise(positionNumber, delayValue, intervalToShow).then(onPromiseresolve).catch(onPromiseReject);
+     positionNumber+=1;
+  intervalToShow+=stapValue;
+    }
+  }, stapValue);
+  setTimeout(()=>{refs.btnStart.disabled=false;
+  },btnDisabledTime(delayValue, positionAmount, stapValue));
   }
+
+function createPromise(position, delay, interval) {
+  const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${interval}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${interval}ms`);
+      }
+    }, delay);
+})}
+
+function onPromiseresolve(result){
+  Notiflix.Notify.success(result, {position: 'center-top', timeout: 5000,});
+};
+
+function onPromiseReject(error){
+  Notiflix.Notify.failure(error, {position: 'center-top', timeout: 5000,});
+}
+
+function btnDisabledTime(delay, amount, stap){
+return delay+amount*stap+5000;
 }
